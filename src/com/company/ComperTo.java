f553193fe6a4857ecb7945214e8c41c2698666fd
Iliassemeonidis <paleologos@mail.ru>
2020-01-24 16:38:42 +0300

    Дзшка с фильмами и музыкой

---
+++ src/com/company/ComperTo.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.TreeSet;
+
+public class ComperTo implements Comparable <ComperTo> {
+
+    @Override
+    public int compareTo(ComperTo o) {
+
+
+        return 0;
+    }
+}
